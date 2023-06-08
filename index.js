f6e6bce318f7d1d7a1f72254270ea8078a67a87e
Christian Clausen <maxclausen@gmail.com>
2023-06-08 14:25:58 +0200

    New version published

--- index.js
+++ index.js
@@ -3,20 +3,74 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.postToRapids = exports.mistService = void 0;
+exports.parseStoreFileContent = exports.parseRetrieveFileContent = exports.parseUploadFileInfo = exports.storeFilesThenEmit = exports.requestFileThenEmit = exports.sendToClient = exports.broadcastToChannel = exports.joinChannel = exports.replyToOrigin = exports.postToRapids = exports.mistService = void 0;
 const axios_1 = __importDefault(require("axios"));
-function mistService(handlers, init) {
+async function mistService(handlers, init) {
     const action = process.argv[process.argv.length - 2];
     const handler = handlers[action];
     if (handler !== undefined) {
         const envelope = JSON.parse(process.argv[process.argv.length - 1]);
-        handler(envelope);
+        handler(getPayload(), envelope);
     }
     else if (init !== undefined)
-        init();
+        await init();
 }
 exports.mistService = mistService;
-function postToRapids(event, payload) {
-    axios_1.default.post(`${process.env.RAPIDS}/${event}`, payload);
+function postToRapids(event, payload, contentType) {
+    return axios_1.default.post(`${process.env.RAPIDS}/${event}`, payload, contentType !== undefined
+        ? {
+            headers: { "Content-Type": contentType },
+        }
+        : {});
 }
 exports.postToRapids = postToRapids;
+function replyToOrigin(payload, contentType) {
+    return postToRapids("$reply", payload, contentType);
+}
+exports.replyToOrigin = replyToOrigin;
+function joinChannel(channel) {
+    return postToRapids("$join", channel);
+}
+exports.joinChannel = joinChannel;
+function broadcastToChannel(to, event, payload) {
+    return postToRapids("$broadcast", { to, event, payload });
+}
+exports.broadcastToChannel = broadcastToChannel;
+function sendToClient(to, event, payload) {
+    return postToRapids("$send", { to, event, payload });
+}
+exports.sendToClient = sendToClient;
+function requestFileThenEmit(file, emitEvent, passthrough) {
+    return postToRapids("$retrieve", { file, emit: emitEvent, passthrough });
+}
+exports.requestFileThenEmit = requestFileThenEmit;
+function storeFilesThenEmit(contents, emitEvent, passthrough) {
+    return postToRapids("$store", { contents, emit: emitEvent, passthrough });
+}
+exports.storeFilesThenEmit = storeFilesThenEmit;
+async function parseUploadFileInfo(p) {
+    let result = JSON.parse((await p).toString());
+    return result;
+}
+exports.parseUploadFileInfo = parseUploadFileInfo;
+async function parseRetrieveFileContent(p) {
+    let result = JSON.parse((await p).toString());
+    return { ...result, content: Buffer.from(result.content.data) };
+}
+exports.parseRetrieveFileContent = parseRetrieveFileContent;
+async function parseStoreFileContent(p) {
+    let result = JSON.parse((await p).toString());
+    return result;
+}
+exports.parseStoreFileContent = parseStoreFileContent;
+function getPayload() {
+    return new Promise((resolve, reject) => {
+        let bufs = [];
+        process.stdin.on("data", (data) => {
+            bufs.push(data);
+        });
+        process.stdin.on("end", () => {
+            resolve(Buffer.concat(bufs));
+        });
+    });
+}
--- index.ts
+++ index.ts
@@ -1,11 +1,27 @@
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 
-type Handler = (envelope: Envelope) => void;
+export module PayloadTypes {
+  export interface UploadFileInfo<T> {
+    files: { originalname: string; name: string; type: string; size: number }[];
+    passthrough: T;
+  }
+  export interface StoreFileInfo<T> {
+    files: string[];
+    passthrough: T;
+  }
+  export interface FileContent<T> {
+    content: { data: number[] };
+    passthrough: T;
+  }
+}
+export type PayloadBuffer = Promise<Buffer>;
 
-export type Envelope = { payload: string; messageId: string; traceId: string };
-export function mistService(
+type Handler = (payloadBuffer: PayloadBuffer, envelope: Envelope) => void;
+
+export type Envelope = { messageId: string; traceId: string };
+export async function mistService(
   handlers: { [action: string]: Handler | undefined },
-  init?: () => void
+  init?: () => Promise<void>
 ) {
   const action = process.argv[process.argv.length - 2];
   const handler = handlers[action];
@@ -13,10 +29,111 @@ export function mistService(
     const envelope: Envelope = JSON.parse(
       process.argv[process.argv.length - 1]
     );
-    handler(envelope);
-  } else if (init !== undefined) init();
+    handler(getPayload(), envelope);
+  } else if (init !== undefined) await init();
 }
 
-export function postToRapids(event: string, payload: any) {
-  axios.post(`${process.env.RAPIDS}/${event}`, payload);
+type RapidsResponse = AxiosResponse<any, any>;
+export function postToRapids(
+  event: "$reply",
+  payload: any,
+  contentType?: string
+): Promise<RapidsResponse>;
+export function postToRapids(
+  event: "$join",
+  payload: string
+): Promise<RapidsResponse>;
+export function postToRapids(
+  event: "$broadcast",
+  payload: { to: string; event: string; payload: any }
+): Promise<RapidsResponse>;
+export function postToRapids(
+  event: "$send",
+  payload: { to: string; event: string; payload: any }
+): Promise<RapidsResponse>;
+export function postToRapids(
+  event: "$retrieve",
+  payload: {
+    file: string;
+    emit: string;
+    passthrough: any;
+  }
+): Promise<RapidsResponse>;
+export function postToRapids(
+  event: "$store",
+  payload: {
+    contents: Buffer[];
+    emit: string;
+    passthrough: any;
+  }
+): Promise<RapidsResponse>;
+export function postToRapids(
+  event: string,
+  payload: any
+): Promise<RapidsResponse>;
+export function postToRapids(
+  event: string,
+  payload: any,
+  contentType?: string
+) {
+  return axios.post(
+    `${process.env.RAPIDS}/${event}`,
+    payload,
+    contentType !== undefined
+      ? {
+          headers: { "Content-Type": contentType },
+        }
+      : {}
+  );
+}
+export function replyToOrigin(payload: any, contentType?: string) {
+  return postToRapids("$reply", payload, contentType);
+}
+export function joinChannel(channel: string) {
+  return postToRapids("$join", channel);
+}
+export function broadcastToChannel(to: string, event: string, payload: any) {
+  return postToRapids("$broadcast", { to, event, payload });
+}
+export function sendToClient(to: string, event: string, payload: any) {
+  return postToRapids("$send", { to, event, payload });
+}
+export function requestFileThenEmit(
+  file: string,
+  emitEvent: string,
+  passthrough: any
+) {
+  return postToRapids("$retrieve", { file, emit: emitEvent, passthrough });
+}
+export function storeFilesThenEmit(
+  contents: Buffer[],
+  emitEvent: string,
+  passthrough: any
+) {
+  return postToRapids("$store", { contents, emit: emitEvent, passthrough });
+}
+
+export async function parseUploadFileInfo<T>(p: Promise<Buffer>) {
+  let result: PayloadTypes.UploadFileInfo<T> = JSON.parse((await p).toString());
+  return result;
+}
+export async function parseRetrieveFileContent<T>(p: Promise<Buffer>) {
+  let result: PayloadTypes.FileContent<T> = JSON.parse((await p).toString());
+  return { ...result, content: Buffer.from(result.content.data) };
+}
+export async function parseStoreFileContent<T>(p: Promise<Buffer>) {
+  let result: PayloadTypes.StoreFileInfo<T> = JSON.parse((await p).toString());
+  return result;
+}
+
+function getPayload() {
+  return new Promise<Buffer>((resolve, reject) => {
+    let bufs: Buffer[] = [];
+    process.stdin.on("data", (data: Buffer) => {
+      bufs.push(data);
+    });
+    process.stdin.on("end", () => {
+      resolve(Buffer.concat(bufs));
+    });
+  });
 }
